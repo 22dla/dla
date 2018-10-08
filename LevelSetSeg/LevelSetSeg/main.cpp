eacbcfa2af6e441e1e15e230db7f7dddf33d0957
22og <e.volkov@cappasity.com>
2018-10-08 19:05:44 +0300

    added processes window

--- LevelSetSeg/LevelSetSeg/main.cpp
+++ LevelSetSeg/LevelSetSeg/main.cpp
@@ -14,13 +14,13 @@ int main( )
 	ofstream out( "out.csv" );
 	Mat img = imread( "tumor.jpg" );//Read image
 	
-	cvtColor( img, img, CV_RGB2GRAY );
+	cvtColor( img, img, CV_BGR2GRAY );
 	Mat init_mask( img.size( ), CV_8UC1 );
 	init_mask.setTo( 0 );
 	init_mask( Rect( 130, 120, 40, 40 ) ).setTo( 255 );
 
 	
-	LevelSetSeg Segmentator( 1000, 30, 200, 0.1 );
+	LevelSetSeg Segmentator( 300, 30, 200, 0.1 );
 	Mat result = Segmentator.simpleseg( img, init_mask );
 		
 	cout << "Time: " << clock( ) / 1000.0 << endl;
