3b03e776cd0148bcafb54584c569e35ff9fe05ae
22og <e.volkov@cappasity.com>
2018-10-24 13:57:39 +0300

    small fix

--- LevelSetSeg/LevelSetSeg/LevelSetSeg.h
+++ LevelSetSeg/LevelSetSeg/LevelSetSeg.h
@@ -8,16 +8,16 @@ public:
 	LevelSetSeg( size_t max_its, int E, int T, double alpha );
 	~LevelSetSeg( );
 
-	cv::Mat simpleseg( cv::Mat I, cv::Mat init_mask );
+	cv::Mat simpleseg( const cv::Mat I, const cv::Mat init_mask );
 
 	double mAlpha;
 	int mT;
 	int mE;
 	size_t mMax_its;
 private:
-	cv::Mat shiftR( cv::Mat M );
-	cv::Mat shiftL( cv::Mat M );
-	cv::Mat shiftU( cv::Mat M );
-	cv::Mat shiftD( cv::Mat M );
-	cv::Mat getCurvature( cv::Mat phi );
+	cv::Mat shiftR( const cv::Mat M );
+	cv::Mat shiftL( const cv::Mat M );
+	cv::Mat shiftU( const cv::Mat M );
+	cv::Mat shiftD( const cv::Mat M );
+	cv::Mat getCurvature( const cv::Mat phi );
 };
--- LevelSetSeg/LevelSetSeg/main.cpp
+++ LevelSetSeg/LevelSetSeg/main.cpp
@@ -11,16 +11,15 @@ using namespace std;
 
 int main( )
 {
-	ofstream out( "out.csv" );
-	Mat img = imread( "tumor.jpg" );//Read image
+	ofstream out( "out.txt" );
+	Mat img = imread( "face.jpg", CV_LOAD_IMAGE_GRAYSCALE );//Read image
 	
-	cvtColor( img, img, CV_BGR2GRAY );
 	Mat init_mask( img.size( ), CV_8UC1 );
 	init_mask.setTo( 0 );
-	init_mask( Rect( 130, 120, 40, 40 ) ).setTo( 255 );
+	init_mask( Rect( 10, 10, 40, 40 ) ).setTo( 255 );
 
 	
-	LevelSetSeg Segmentator( 300, 30, 200, 0.1 );
+	LevelSetSeg Segmentator( 10000, 30, 255, 0.1 );
 	Mat result = Segmentator.simpleseg( img, init_mask );
 		
 	cout << "Time: " << clock( ) / 1000.0 << endl;
